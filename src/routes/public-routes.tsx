c920d75b9745c4acfd7f323d2f43d7851165c01d
FemboyIRL <gatoelluis@gmail.com>
2025-03-01 16:48:58 -0700

    first commit

---
+++ src/routes/public-routes.tsx
@@ -0,0 +1,18 @@
+export const outSidePages = [
+    {
+      path: "/",
+      element: <></>
+    },
+    {
+      path: "/home",
+      element: <></>,
+    },
+    {
+      path: "/login",
+      element: <></>,
+    },
+    {
+      path: "/set_password/:token",
+      element: <></>,
+    },
+  ];
